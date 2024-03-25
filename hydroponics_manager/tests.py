ca09fd07abe63be275d7d07d61732d79cdcfdc18
MatRos-sf <mateuszrosenkranz@gmail.com>
2024-03-25 19:48:48 +0100

    create simple test

--- hydroponics_manager/tests.py
+++ hydroponics_manager/tests.py
@@ -1,3 +1,18 @@
-from django.test import TestCase
+from django.contrib.auth.models import User
+from django.test import TestCase, tag
 
-# Create your tests here.
+from .factories import HydroponicSystemFactory, MeasurementFactory, UserFactory
+from .models import HydroponicSystem, Measurement
+
+
+class HydroponicSystemTest(TestCase):
+    @classmethod
+    def setUpClass(cls):
+        UserFactory.create_batch(3)
+
+    def test_should_create_3_users(self):
+        self.assertEqual(User.objects.count(), 3)
+
+    def test_should_create_3_hydroponicsystems(self):
+        HydroponicSystemFactory.create_batch(3)
+        self.assertEqual(HydroponicSystem.objects.count(), 3)
