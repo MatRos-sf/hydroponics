3a03e046b81aaba1f2afc98d09538838d161c7c3
MatRos-sf <mateuszrosenkranz@gmail.com>
2024-03-25 18:51:41 +0100

    create models
    + HydroponicSystem
    + Measurement

--- hydroponics_manager/models.py
+++ hydroponics_manager/models.py
@@ -1,2 +1,26 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.core.validators import MinValueValidator, MaxValueValidator
+
+
+class HydroponicSystem(models.Model):
+    """
+    Model presents hydroponic system
+    """
+    name = models.CharField(max_length=150)
+    description = models.TextField(blank=True, null=True)
+    owner = models.ForeignKey(User, on_delete=models.CASCADE, related_name='systems')
+    created = models.DateTimeField(auto_now_add=True)
+    is_active = models.BooleanField(default=True)
+
+
+class Measurement(models.Model):
+
+    measurement_date = models.DateTimeField(auto_now_add=True)
+    ph = models.FloatField(blank=True, null=True, validators=[MinValueValidator(0.0), MaxValueValidator(14)])
+    water_temperature = models.FloatField(blank=True, null=True, validators=[MinValueValidator(0), MaxValueValidator(100)],
+                                          help_text="Water temperature is measured in degrees Celsius")
+    tds = models.FloatField(blank=True, null=True, help_text="TDS is present in mg/L units")
+    hydroponic_system = models.ForeignKey(HydroponicSystem, on_delete=models.CASCADE, related_name="measurements")
+
 
