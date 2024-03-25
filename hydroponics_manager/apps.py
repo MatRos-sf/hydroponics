f25785b5eed400d6d3d6f57cfaf4012407136494
MatRos-sf <mateuszrosenkranz@gmail.com>
2024-03-25 18:53:26 +0100

    launch pre-commit

--- hydroponics_manager/apps.py
+++ hydroponics_manager/apps.py
@@ -2,5 +2,5 @@ from django.apps import AppConfig
 
 
 class HydroponicsManagerConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'hydroponics_manager'
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "hydroponics_manager"
